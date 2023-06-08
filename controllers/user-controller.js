1d24a95c71440d11efb2823f5a104f802cac21ba
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-06-08 02:26:34 -0400

    added function to delete friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -94,5 +94,19 @@ updateUser({ params, body }, res) {
       })
       .catch((err) => res.json(err));
   },
-
-}
+ // delete friend
+ removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No user with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
