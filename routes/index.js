e582558bddb805c3ea5f2bc930f7fdf0f2b47e27
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-06-08 03:16:50 -0400

    updated code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-router.use("/api", apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).send("ERROR WRONG ROUTE! error 404");
-});
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
